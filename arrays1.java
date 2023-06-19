cf81c5362b5bce6f8fac04d3729ccf96a484dd8c
Mrudul Mohan <mrudulmohan2001@gmail.com>
2023-06-20 00:08:00 +0530

    second commit

---
+++ arrays1.java
@@ -0,0 +1,28 @@
+//simple java array program to declare and print elements of the array by traversing through it.3
+/*
+class Testarray{
+    public static void main(String[] args) {
+        int achu[]=new int[5];
+        achu[0]= 10;
+        achu[1]=20;
+        achu[2]=30;
+        achu[3]=40;
+        achu[4]=50;
+
+
+        for(int i=0;i< achu.length;i++){
+            System.out.println(achu[i]);
+        }
+    }
+}*/
+
+class Testarray{
+    public static void main(String[] args) {
+        int achu[]= {1,2,3,4,5};
+        for(int i=0; i<achu.length;i++){
+            System.out.println(achu[i]);
+        }
+    }
+
+
+}
---
+++ twonumbersum.java
@@ -0,0 +1,20 @@
+public class twonumbersum{
+    public static int[] twoNumberSum(int[] array,int target){
+        for(int i=0;i<array.length;i++){
+            for(int j=0;j<array.length;j++){
+                if (array[i]+array[j]==target){
+                    return new int[]{array[i],array[j]};
+                }
+            }
+        }
+        return new int[0];
+    }
+    public static void main (String[]args){
+        int[] array = {6, 5, 7, 9, 4, 0, 2};
+        int target = 10;
+        int[] result = twoNumberSum(array, target);
+        for (int i = 0;i<result.length;i++) {
+            System.out.println(result[i]);
+        }
+    }
+}
