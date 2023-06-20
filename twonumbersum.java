3057eeb02351743783ffff99a7bfeb2e9387e2fc
Mrudul Mohan <mrudulmohan2001@gmail.com>
2023-06-20 11:40:16 +0530

    minute change

--- twonumbersum.java
+++ twonumbersum.java
@@ -1,7 +1,7 @@
 public class twonumbersum{
     public static int[] twoNumberSum(int[] array,int target){
         for(int i=0;i<array.length;i++){
-            for(int j=0;j<array.length;j++){
+            for(int j=i+1;j<array.length;j++){
                 if (array[i]+array[j]==target){
                     return new int[]{array[i],array[j]};
                 }
