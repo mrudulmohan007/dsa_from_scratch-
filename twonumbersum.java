81c62834dd8cb293c781476c38ca83c625156a65
Mrudul Mohan <mrudulmohan2001@gmail.com>
2023-06-21 22:50:02 +0530

    palindrome code added

--- twonumbersum.java
+++ twonumbersum.java
@@ -1,6 +1,6 @@
 public class twonumbersum{
     public static int[] twoNumberSum(int[] array,int target){
-        for(int i=0;i<array.length;i++){
+        for(int i=0;i<array.length-1;i++){
             for(int j=i+1;j<array.length;j++){
                 if (array[i]+array[j]==target){
                     return new int[]{array[i],array[j]};
