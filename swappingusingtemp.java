a0498bdb5d9d6102941bbc6c1180616cf10809c6
Mrudul Mohan <mrudulmohan2001@gmail.com>
2023-06-27 23:29:18 +0530

    2 codes added(swapping)

---
+++ swappingusingtemp.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class swappingusingtemp {
+    public static void main(String[] args) {
+        int x= 10;
+        int y= 20;
+        int temp;
+        temp=x;
+         x=y;
+        y=temp;
+        System.out.print("result is : " + x +" "+y);
+    }
+}
+
+//swapping 2 numbers using temp variable.
---
+++ swapw/ousingtemp.java
@@ -0,0 +1,13 @@
+package swapw;
+
+import java.util.*;
+public class ousingtemp {
+    public static void main(String[] args) {
+       int x=10;
+       int y=20;
+       x=x+y;
+       y=x-y;
+       x=x-y;
+        System.out.println("value of X = " +x+" value of Y = " +y);
+    }
+}
