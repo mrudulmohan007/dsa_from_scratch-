0d6382473fb5c9a9d4daea3669036bda58a7cffe
Mrudul Mohan <mrudulmohan2001@gmail.com>
2023-08-29 12:29:33 +0530

    swap elements in array

---
+++ Arrays/swappingtheelementsofarray.java
@@ -0,0 +1,16 @@
+package Arrays;
+
+import java.util.*;
+public class swappingtheelementsofarray {
+    static void swap(int[] arr,int index1,int index2){
+        int temp= arr[index1];
+        arr[index1]=arr[index2];
+        arr[index2]=temp;
+    }
+
+    public static void main(String[] args) {
+        int[] arr={1,2,3,4,5,6};
+        swap(arr,1,2);
+        System.out.println(Arrays.toString(arr));
+    }
+}
